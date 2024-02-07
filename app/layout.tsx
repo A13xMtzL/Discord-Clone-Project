40e06069345f81229c29c9c9aadcc7c0d2deb51d
Alejandro Martínez Luna <A01276785@tec.mx>
2024-02-06 19:16:37 -0600

    Added theme provider and class utility in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider } from '@/components/providers/theme-provider';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { cn } from '@/lib/utils';
 
-const open_sans = Open_Sans({ subsets: ["latin"] });
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Discord Clone",
@@ -18,7 +20,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={open_sans.className}>{children}</body>
+        <body className={cn
+          (font.className,
+            "bg-white dark:bg-gray-900"
+          )}>
+          <ThemeProvider
+            attribute='class'
+            defaultTheme='dark'
+            enableSystem={true}
+            storageKey='discord-theme'
+          >{children}</ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
